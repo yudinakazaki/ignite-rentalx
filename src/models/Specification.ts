0b046d52fd1d1ebacc550e4d0f8a6c8d215b3f64
Yudi Nakazaki <yudi.nakazaki@provi.com.br>
2021-06-02 18:53:28 -0300

    first commit

---
+++ src/models/Specification.ts
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid'
+
+class Specification {
+  id?: string
+  name: string
+  description: string
+  createdAt: Date
+
+  constructor(name: string, description: string) {
+    if (!this.id) {
+      this.id = uuidv4()
+    }
+
+    this.name = name
+    this.description = description
+    this.createdAt = new Date()
+  }
+}
+
+export { Specification }
---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,26 @@
+import { Category } from '../models/Category'
+import { ICreateCategoryDTO } from './ICategoriesRepository'
+
+class CategoriesRepository {
+  private categories: Category[]
+
+  constructor() {
+    this.categories = []
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category(name, description)
+
+    this.categories.push(category)
+  }
+
+  list(): Category[] {
+    return this.categories
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name)
+  }
+}
+
+export { CategoriesRepository }
